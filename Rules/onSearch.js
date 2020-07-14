74becaed61a43773957dfdb37b8c1e6d718af97c
Pany Pan <pany.pan@sap.com>
2020-07-14 05:07:35 +0000

    add map control - 7

--- Rules/onSearch.js
+++ Rules/onSearch.js
@@ -21,7 +21,7 @@
 	var mapControl = context.evaluateTargetPath("#Page:Search/#Control:MapControlinSearch");
 	// logger.log(mapControl);
 	logger.log("before add");
-	mapControl.__proto__.setValue(anotherStore);
+	mapControl._extension.setValue(anotherStore);
 	logger.log("after add");
 	// logger.log(esName);
 	// alert(esName);
